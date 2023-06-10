451d6cf846945ea5202043c0095c8a93f265cec2
keshav <ksv508@gmail.com>
2023-06-10 09:50:26 +0530

    first Submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args){
+        int x = 1,y = 1,z = 1;
+        double a= 1,b = 1;
+        Product p = new Product();
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product(a,b));
+    }
+    public static class Product{
+        public int product(int x,int y) {
+            return x+y;
+        }
+        public int product(int x, int y, int z){
+            return x+y+z;
+        }
+        public double product(double x, double y){
+            return x+y;
+        }
+    }
 }
