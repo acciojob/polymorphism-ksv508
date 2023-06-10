67b869d5e5f07053269c111e95ed068171262996
keshav <ksv508@gmail.com>
2023-06-10 10:02:05 +0530

    resubmit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Product p = new Product();
         p.product(1,1);
         p.product(1,1,1);
-        p.product(1,1);
+        p.product(1.0,1.0);
     }
     public static class Product{
         public int product(int x,int y) {
